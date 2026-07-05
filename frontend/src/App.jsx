61c08711fffaddcb7a59ecab8d74a62f1fcec27a
james <beheramithun2021@gamil.com>
2026-07-06 00:57:11 +0530

    create Project Folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Unity Foundation Trust</h1>
+      <p className="lead">Serving Humanity Through Unity</p>
+
+      <button className="btn btn-primary">
+        Bootstrap Working
+      </button>
+    </div>
+  );
+}
+
+export default App;
