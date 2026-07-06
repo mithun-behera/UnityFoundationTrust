d517a6daff55788f5f62a184640efdcadc590000
james <beheramithun2021@gamil.com>
2026-07-06 12:38:18 +0530

    comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+
 import Dashboard from "./pages/Dashboard";
-import Donors from "./pages/Donors"
-import Beneficaries from "./pages/Beneficiaries";
-import Donations from "./pages/compaigns";
+import Donors from "./pages/Donors";
+import Beneficiaries from "./pages/Beneficiaries";
+import Donations from "./pages/Donations";
+import Campaigns from "./pages/Campaigns";
 import Volunteers from "./pages/Volunteers";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
-import Campaigns from "./pages/compaigns";
+
 function App() {
   const [activePage, setActivePage] = useState("Dashboard");
-  function showPage(){
-    if(activePage === "Dashboard") return <Dashboard/>;
-    if(activePage === "Donors") return <Donors/>;
-    if(activePage === "Campaigns") return <Campaigns/>;
-    if(activePage === "Volunteers") return <Volunteers/>;
-    if(activePage === "Reports") return <Reports />;
-    if(activePage === "Settings") return <Settings/>;
+
+  function showPage() {
+    if (activePage === "Dashboard") return <Dashboard />;
+    if (activePage === "Donors") return <Donors />;
+    if (activePage === "Beneficiaries") return <Beneficiaries />;
+    if (activePage === "Donations") return <Donations />;
+    if (activePage === "Campaigns") return <Campaigns />;
+    if (activePage === "Volunteers") return <Volunteers />;
+    if (activePage === "Reports") return <Reports />;
+    if (activePage === "Settings") return <Settings />;
   }
+
   return (
     <div className="d-flex min-vh-100 bg-dark text-white w-100">
       <Sidebar activePage={activePage} setActivePage={setActivePage} />
-       <main className="flex-grow-1">
-          <Navbar/>
-          {showPage}
-       </main>
+
+      <main className="flex-grow-1">
+        <Navbar />
+        {showPage()}
+      </main>
     </div>
   );
 }
