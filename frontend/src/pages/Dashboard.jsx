3c3d36d28d62dfd8a1a3466123cbd779dd1a980b
james <beheramithun2021@gamil.com>
2026-07-06 15:18:57 +0530

    recent donors

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
-import{ Row, Col } from "react-bootstrap";
+// import{ Row, Col } from "react-bootstrap";
+import {Row, Col, Card, Table, Badge} from "react-bootstrap";
 import {
   FaUsers,
   FaDonate,
@@ -33,6 +34,52 @@ function Dashboard(){
              <DashboardCard title="Active Campaigns" value="9" icon={<FaBullhorn/>} color="warning"/>
            </Col>
         </Row>
+          <Row className="mt-4">
+             <Col lg={8}>
+             <Card bg="dark" text="white" className="border-secandary">
+                 <Card.Body>
+                    <Card.Title className="mb-4">Recent Donations</Card.Title>
+
+                    <Table responsive hover variant="dark" className="mb-0">
+                        <thead>
+                          <tr>
+                            <th>Donor Name</th>
+                            <th>Donation Type</th>
+                            <th>Amount</th>
+                            <th>Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Om Prakash</td>
+                            <td>Money</td>
+                            <td>₹ 60,500</td>
+                            <td><Badge bg="success">Approved</Badge></td>
+                          </tr>
+
+                          <tr>
+                            <td>Mithun </td>
+                            <td>Food</td>
+                            <td>₹ 40,000</td>
+                            <td>
+                              <Badge bg="warning" text="dark">Pending</Badge>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>Amit Kumar</td>
+                            <td>Books</td>
+                            <td>₹ 45,000</td>
+                            <td> 
+                              <Badge bg="success">Approved</Badge>
+                            </td>
+                          </tr>
+                        </tbody>
+                    </Table>
+                 </Card.Body>
+             </Card>
+             </Col>
+          </Row>
     </div>
   );
 }
